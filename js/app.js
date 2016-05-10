aa22e714b594a273114089870ad9b05a0740af11
radhikajaju <radhikajaju1702@gmail.com>
2016-05-10 17:01:05 +0530

    doc:adding functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 /*
  * description Object with different locations from the neighboourhood.
- * description Some variables are added but not used (for now)
+ * description Some variables are added but latter
  */
 var locations = [{
-    locationName: 'Cappuccino Mini Blast',
-    streetName: 'Eldeco Greens, Near Fun Republic Mall',
-    zipCode: '226001',
+    locationName: 'Gomti Nagar Greens',
+    streetName: 'Gomti Nagar Railway Station Road, Vivek Khand, Near Neelkanth Sweets, Gomtinagar, Lucknow, Uttar Pradesh',
+    zipCode: ' 226010',
     city: 'Lucknow',
     description: 'Excellent Cafe, with unique interiors',
-    Lati: 26.835361,
-    Long: 80.947542, 
-    id: 'cappuccino'
+    Lati: 26.862585, 
+    Long: 80.997039, 
+    id: 'gomtigreen'
 }, {
     locationName: 'JJ Bakers',
     streetName: 'Vijay Khand, Ujariyaon, Vivek Khand 4,Gomti Nagar',
@@ -65,48 +65,58 @@ mapObj.initMap = function() {
         },
         zoom: 12
     });
-    $('#map').css('height', '400px'); //overrides Bootstrap settings
+    $('#map').css('height', '400px');
     mapObj.createMarkers();
     mapObj.setAllMarkers(self.canvasMap);
     mapObj.createInfoWindow();
 };
-/*Create the markers and its definitions, Calculate each marker based on its given Lattitude and Longitude*/
+
+/**
+ * description Create the markers and its definitions
+ * description Calculate each marker based on its given Lattitude and Longitude
+ * description Also sets each title, description and city name, in my case Lucknow,India
+ */
 mapObj.createMarkers = function() {
     'use strict';
+
     var self = this;
     self.marker = null;
     self.markerArray = ko.observableArray();
+
     self.displayAllMarkers = ko.computed(function() {
-        //loop through the locations and create markers
+        //loop through the locations and create markers.
         for (var i = 0; i < locations.length; i++) {
             var LatLng = {
                 lat: locations[i].Lati,
                 lng: locations[i].Long
             };
-            self.marker = new google.maps.Marker({ 
-            //create the markers
+            self.marker = new google.maps.Marker({ //create the markers
                 position: LatLng,
                 title: locations[i].locationName,
                 description: locations[i].description,
                 city: locations[i].city,
             });
-            self.markerArray.push(self.marker); //push markers in an array
+            self.markerArray.push(self.marker); //storing markers in an array
 
             mapObj.openWindow(self.marker, false);
             mapObj.bounceMarker(self.marker, false);
-        } //end for
+        }
 
-    }, self); //end displayAllMarkers
+    }, self);
 };
 
-//description Takes all the markers from the MarkerArray and puts them onto the map
- mapObj.setAllMarkers = function(map) {
+/**
+ * description Takes all the markers from the MarkerArray and puts them on the map
+ * @param {object} - map - Google Map from initMap()
+ */
+mapObj.setAllMarkers = function(map) {
+
     for (var i = 0; i < locations.length; i++) {
         this.markerArray()[i].setMap(map);
     }
-}; //end setAllMarkers
+};
 
-//Create a Google Map info (popup) window
+/** Create a Google Map popup window */
 mapObj.createInfoWindow = function() {
     this.infowindow = new google.maps.InfoWindow({
         content: '<div id="windowTool"></div>'
@@ -119,7 +129,7 @@ mapObj.createInfoWindow = function() {
  * @param {boolean} status - Status: Sidebar selection or map selection
  */
 
-mapObj.openWindow = function(marker, status) { //open the GM tooltip
+mapObj.openWindow = function(marker, status) {
     var self = this;
     //listen to a click and then open the correct window for that marker
     if (status === false) {
@@ -137,17 +147,15 @@ mapObj.openWindow = function(marker, status) { //open the GM tooltip
      * @description Set and show the title and the description
      * @description launch funtion displayPhoto from the photo Model
      */
-//window.infoWindow.setContent($('#placeTmpl').html());
-
     function openWin() {
-        //self.infowindow.close(); //close other infowindows first
+        self.infowindow.close(); //close other infowindows first
         self.infowindow.open(self.canvasMap, marker); //open the window
-        //$('#windowTool').empty(); //empty everything first
-        //$('window.infoWindow.setContent($(.marker_title).html())' + marker.title + '</h4>' +
-          //  '<h5>' + marker.description + '</h5></div>').appendTo('#windowTool'); //now add data
+        $('#windowTool').empty(); //empty everything first
+        $('<div class="marker_title"><h4>' + marker.title + '</h4>' + '<h5>' + marker.description + '</h5></div>').appendTo('#windowTool'); //now add data
+        //self.infowindow.setContent( '<div><h4>' + marker.title + '</h4>' + '<h5>' + marker.description + '</h5></div>');// unable to display image using knockoutJS
+        photoObj.displayPhoto(marker); //get the photos from the internet
 
-        self.infowindow.setContent( '<div><h4>' + marker.title + '</h4>' + '<h5>' + marker.description + '</h5></div>');
-        }
+    }
 };
 
 /**
@@ -156,7 +164,7 @@ mapObj.openWindow = function(marker, status) { //open the GM tooltip
  * @param {boolean} status - Status: Sidebar selection or map selection
  */
 mapObj.bounceMarker = function(marker, status) {
-    if (status === false) {
+	if (status === false) {
         marker.addListener('mouseover', function(markerON) {
             //bounce the marker when mouseover the marker on the map
             bounce(marker);
@@ -165,13 +173,14 @@ mapObj.bounceMarker = function(marker, status) {
         //bounce the marker when mouseover over the list items
         bounce(marker);
     }
+
     /**
      * @description Bounce the selected marker.
      * @param {object} marker - Selected Map Marker object
      */
     function bounce(marker) {
         marker.setAnimation(google.maps.Animation.BOUNCE);
-        //bounce the actual marker and unbounce it after a couple of sec.
+        //bounce the actual marker and unbounce it after few sec.
         window.setTimeout(function() {
             unbounce(marker);
         }, 2500);
@@ -185,44 +194,53 @@ mapObj.bounceMarker = function(marker, status) {
     }
 };
 
-//@description creation of the Sidebar Model
-
+/**
+ * @description creation of the Sidebar Model
+ */
 var sidebarObj = {};
-//@description Initialize the sidebar Model
- sidebarObj.init = function() {
+
+/**
+ * @description Initialize the sidebar Model
+ */
+sidebarObj.init = function() {
+
     this.places = ko.observableArray(locations.slice(0)); //array to hold the locations
     this.query = ko.observable(''); //oberve the search field
+
 };
-sidebarObj.init(); //launch it
+sidebarObj.init();
 
 /**
  * @description Search function to search through the locations
  * @param {object} marker - Selected Map Marker object
  * @param {boolean} status - Status: Sidebar selection or map selection
  */
-//
 sidebarObj.searchLoc = function() {
     var self = this;
+
     /**
      * @description Search function to search through the locations
      * @param {object} value - takes the value from the searchbar
      *
      */
     self.query.subscribe(search = function(value) {
-        self.places.removeAll(); // remove all the current places, which removes them from the view
+        self.places.removeAll(); // remove other current places
         mapObj.setAllMarkers(null); //also remove the markers from the map
+
         for (var i = 0; i < locations.length; i++) {
-        //iterate through the locations to find the query value (the name of the location)
+            //iterate through the locations to find the query value (the name of the location)
             if (locations[i].locationName.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                 self.places.push(locations[i]);
+
                 self.marker = mapObj.markerArray()[i]; //get the marker data from the array in the mapModel
                 self.marker.setMap(mapObj.canvasMap); //set the markers back onto the map
-            } 
+            }
         } 
-    }); 
+    });
 }; 
 
-sidebarObj.searchLoc(); //activate the searchLoc function
+sidebarObj.searchLoc(); 
+
 /**
  * @description When a location in the sidebar is highlighted with the mouse we know what marker corresponds with that name
  * @description Do the mouse over and click (when clicked) and bounce the marker and open a window
@@ -233,10 +251,8 @@ sidebarObj.selectLocation = function(element, domEl) { //perform some styling.
     console.log('element', element);
     console.log('domEl', domEl);
     var current = domEl.currentTarget;
-    //sidebarObj.bounceMarker(element.locationName);
     showMarker(element.locationName);
-
-    //show the marker that is selected based on the location name
+	//show the marker that is selected based on the location name
     function showMarker(locationName) {
         for (var i = 0; i < mapObj.markerArray().length; i++) {
             if (mapObj.markerArray()[i].title.toLowerCase().indexOf(locationName.toLowerCase()) >= 0) {
@@ -262,5 +278,86 @@ sidebarObj.openWin = function(element, domEl) {
         }
     }
 };
+
+/**
+ * @description When the users clicks a marker this function (or model) is run to display photos below the map
+ */
+var photoObj = {
+    showFlickr: ko.observable(true),
+    urls_flickr: ko.observableArray(),
+    noImg: ko.observable(false),
+    errorImg: ko.observable(false),
+};
+/**
+ * @description If there is a location found please go ahead and print the pictures
+ * @param { object} [marker] [Google Maps Marker]
+ */
+photoObj.displayPhoto = function(marker) {
+    var locationName = marker.title;
+
+    if (locationName === undefined) {
+        return false;
+    } else { //only perform action if there is a location set
+
+        photoObj.flickr(marker);
+    }
+};
+
+/**
+ * @description Retrieve images from Flickr using its API. The search is done base on the title of the marker
+ * @param { object} [marker] [Google Maps Marker]
+ */
+photoObj.flickr = function(marker) {
+    var self = this;
+    var locationName = marker.title;
+    self.urls_flickr([]);
+
+    $('#flickrPhotos').empty(); //empty all the previous photo's
+    var string = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=be17caa754fcee058bb87de7f2d281aa&jsoncallback=?';
+    $.getJSON(string, {
+            text: locationName,
+            tagmode: 'any',
+            sort: 'interestingness-desc',
+            has_geo: 1,
+            extras: 'description',
+            format: 'json'
+        },
+        function(data) {
+
+            if (data.stat !== 'fail') { //check if there is no failure
+
+                for (var i = 0; i < data.photos.photo.length; i++) {
+
+                    //check if the locationName is in the title && in the description. If so, it's a valid photo
+                    var title_check = data.photos.photo[i].title.toLowerCase().indexOf(locationName.toLowerCase());
+                    var decript_check = data.photos.photo[i].description._content.indexOf(locationName.toLowerCase());
+
+                    if (title_check || decript_check >= 0) {
+                        var photosChecked = data.photos.photo[i];
+                        var photoUrl = 'https://farm' + photosChecked.farm + '.staticflickr.com/' + photosChecked.server + '/' + photosChecked.id + '_' + photosChecked.secret + '.jpg' + '" >';
+                        self.urls_flickr.push({
+                            url: photoUrl
+                        }); //push photos in an array again so that we can use them in the html
+                    }
+                }
+
+                var randPic = self.urls_flickr()[Math.floor(Math.random() * self.urls_flickr().length)]; //randomizer
+                console.log(randPic);
+                $('<img/>').attr('src', randPic.url).attr('id', 'picP').appendTo('#windowTool');
+            } else {
+                photoObj.errorImg(true);
+                $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#windowTool');
+            }
+        }
+
+
+    );
+
+    $(document).ajaxError(function() {
+        $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#gettyPhotos');
+    });
+
+};
+
 ko.applyBindings(mapObj, document.getElementById('map'));
 ko.applyBindings(sidebarObj, document.getElementById('sidebar'));
